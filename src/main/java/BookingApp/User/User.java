ea9efaf1e304de6879f42b5a7bd56dc322768e53
shuiu96@gmail.com <shuiu96@gmail.com>
2017-08-07 17:01:46 +0300

    added email

--- src/main/java/BookingApp/User/User.java
+++ src/main/java/BookingApp/User/User.java
@@ -8,14 +8,22 @@ public class User {
 @Id
 	private String id;
 	private String password;
+	private String email;
 	public User()
 	{
 		
 	}
-	public User(String id, String password) {
+	public User(String id, String password,String email) {
 		super();
 		this.id = id;
 		this.password = password;
+		this.email=email;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getId() {
 		return id;
