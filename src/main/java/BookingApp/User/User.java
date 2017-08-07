a56454a3b3a54bb746f35c740aededf57a5b482a
shuiu96@gmail.com <shuiu96@gmail.com>
2017-08-07 16:08:07 +0300

    user class maybe

---
+++ src/main/java/BookingApp/User/User.java
@@ -0,0 +1,33 @@
+package BookingApp.User;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+@Id
+	private String id;
+	private String password;
+	public User()
+	{
+		
+	}
+	public User(String id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
