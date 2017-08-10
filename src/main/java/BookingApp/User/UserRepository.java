703ad1da52af95d5627532c315c18c13b2acd73f
shuiu96@gmail.com <shuiu96@gmail.com>
2017-08-10 13:53:35 +0300

    slight fix

--- src/main/java/BookingApp/User/UserRepository.java
+++ src/main/java/BookingApp/User/UserRepository.java
@@ -1,5 +1,5 @@
 package BookingApp.User;
 import org.springframework.data.repository.CrudRepository;
-public interface UserRepository extends CrudRepository<User,String> {
+public interface UserRepository extends CrudRepository<User,Long> {
 
 }
